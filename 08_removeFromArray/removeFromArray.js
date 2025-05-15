89de5c84867fee31952ac33912b7d1b8e71388c4
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-15 23:59:49 +0800

    Tests passed
    I was staring at the abyss then suddenly I put
    j-- and voila all tests passed. Jokes aside,
    watched a couple of youtube tutorials and read
    documentation on spread syntax and rest params.
    
    Note to self: when using loops on arrays always
    consider mutation of arrays. ALWAYS

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...num) {
+    for(j = 0; j < array.length; j++) {
+        for(i = 0; i < num.length; i++) {
+            if(array[j] === num[i]) {
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
