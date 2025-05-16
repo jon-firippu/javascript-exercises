fb56f9441c9aa40a45c8d602cc090d9ccacd7225
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-16 23:12:47 +0800

    Tests passed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    let sum = 0;
+    
+    if (Number.isInteger(first) && Number.isInteger(second) && first > 0 && second > 0) {
+        if (first < second) {
+            for (i = first; i < second; i++) {
+                sum += i;
+            }
+            return sum + second;
+        }
+        else {
+            for (i = second; i < first; i++) {
+                sum += i;
+            }
+            return sum + first;
+        }
+    }
+    return "ERROR";
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
