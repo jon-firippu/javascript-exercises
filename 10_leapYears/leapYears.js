36a6fcb79ac8e4fbf761cf4c73e1a6dda305fe8e
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-21 16:57:11 +0800

    Tests passed
    Changed my approach by nesting if statements
    I'm not sure why I can't incorporate && to make
    all tests pass.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,14 +1,17 @@
-const leapYears = function(year) {
-    
-    if(year % 100 === 0 && year % 400 === 0) {
-        return true;
+const leapYears = function (year) {
+
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    else if (year % 4 === 0) {
+    if (year % 4 === 0) {
         return true;
     } else {
         return false;
     }
-    
 };
 
 // Do not edit below this line
