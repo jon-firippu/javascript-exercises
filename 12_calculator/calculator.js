e36a522d52907de109336d201372b8739f43b660
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-24 21:12:44 +0800

    Tests passed
    so in javascript power operator is "**" or maybe
    on others as well. I forgot. Quick google always
    says Math.pow(), but I wrote "^" on the first try
    for some reason.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length === 0) {
+    return 0;
+  }
+  if (array.length === 1) {
+    return array[0];
+  }
+  let sum = 0;
+  for (i = 0 ; i < array.length ; i++) {
+    sum += array[i];
+  }
+  return sum;
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (i = 0 ; i < array.length ; i++) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  }
+  let factorial = 1;
+  for (i = number ; i > 0 ; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
