cca2260a4994b8b034ff920fe17a0088b5e6f4da
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-25 20:41:40 +0800

    Tests passed
    After checking solution and incorporating the idea
    on the existing code I was able to pass all tests.
    
    BUT I'm still confused so gonna review it later.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,18 +1,12 @@
 const palindromes = function (str) {
     //remove white spaces and punctuations
-    const strCleaned = str.split('').filter(char => {
-        /[a-zA-Z0-9]/.test(char);
-    }).join('');
+    const strCleaned = str.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '');
     //reverse the string
     let reversed = "";
     for (i = strCleaned.length - 1 ; i >= 0 ; i--) {
         reversed += strCleaned[i];
     }
-    if (strCleaned === reversed) {
-        return true;
-    } else {
-        return false;
-    }
+    return strCleaned === reversed;
 };
 
 // Do not edit below this line
