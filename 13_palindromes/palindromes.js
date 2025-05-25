889d663effddb792426e28a2096505fea02a0887
jon-firippu <177222260+jon-firippu@users.noreply.github.com>
2025-05-25 19:45:14 +0800

    Incomplete
    ✕ doesn't just always return true
    ✕ works with unevenly spaced numbers in a string

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    //remove white spaces and punctuations
+    const strCleaned = str.split('').filter(char => {
+        /[a-zA-Z0-9]/.test(char);
+    }).join('');
+    //reverse the string
+    let reversed = "";
+    for (i = strCleaned.length - 1 ; i >= 0 ; i--) {
+        reversed += strCleaned[i];
+    }
+    if (strCleaned === reversed) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
